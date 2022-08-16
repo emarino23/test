38f2f71b991b6e73fc4f81e96cb0822245dcec5b
emarino23 <naver.developer2@gmail.com>
2022-08-16 11:47:53 -0500

    delete django

--- admin/src/pantalla/catalogs.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-class UserType(models.IntegerChoices):
-    Admin = 1, _('Admin')
-    Agent = 2, _('Agent')
-    Experience = 3, _('Experience')
-    Travel = 4, _('Travel') 
